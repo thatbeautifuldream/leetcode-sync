29b23a819df26af239c35f0f71776dad11799ef9
Milind Mishra ⚛️ <milind.mishra4@gmail.com>
2021-08-26 20:22:46 +0000

    Sync LeetCode submission - Transpose Matrix (java)

---
+++ practice/problems/transpose_matrix/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int[][] transposeMatrix = new int[matrix[0].length][matrix.length];
+        for(int i = 0; i < matrix[0].length; i++) {
+            for(int j = 0; j < matrix.length; j++) {
+                transposeMatrix[i][j] = matrix[j][i];
+            }
+        }
+        return transposeMatrix;
+    }
+}
