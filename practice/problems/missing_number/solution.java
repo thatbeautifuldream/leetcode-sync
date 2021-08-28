e686e41f8f67af2a8611e42e691618f445178087
Milind Mishra ⚛️ <milind.mishra4@gmail.com>
2021-08-28 19:37:37 +0000

    Sync LeetCode submission - Missing Number (java)

---
+++ practice/problems/missing_number/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int i = 0;
+        while(i<nums.length) {
+            int correct = nums[i];
+            if(nums[i] < nums.length && nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+            else {
+                i++;
+            }
+        }
+        
+        // search missing number
+        for(int index = 0; index < nums.length ; index++) {
+            if(nums[index] != index) {
+                return index;
+            }
+        }
+        return nums.length;
+    }
+}
