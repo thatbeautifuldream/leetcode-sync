ac331d929246e54a648136825b98f0d19afb1059
Milind Mishra ⚛️ <milind.mishra4@gmail.com>
2022-03-10 17:05:29 +0000

    Sync LeetCode submission - Valid Parentheses (java)

---
+++ practice/problems/valid_parentheses/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '{' || ch == '[' || ch == '(') {
+                st.push(ch);
+            }
+            else {
+                if(st.size() == 0) {
+                    return false;
+                }
+                if(ch == '}' && st.peek() != '{') {
+                    return false;
+                }
+                else if(ch == ']' && st.peek() != '[') {
+                    return false;
+                }
+                else if(ch == ')' && st.peek() != '(') {
+                    return false;
+                }
+                else {
+                    st.pop();
+                }
+            }
+        }
+        if(st.size() == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
